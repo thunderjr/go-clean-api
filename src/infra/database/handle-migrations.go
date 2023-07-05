781e7907ccc7fe3e3cc9419e920055fc875b1576
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-07-04 23:21:31 -0300

    fix: database migration using go routines

--- src/infra/database/handle-migrations.go
+++ src/infra/database/handle-migrations.go
@@ -1,7 +1,6 @@
-package migrations
+package infra_database
 
 import (
-	"log"
 	"sync"
 
 	"github.com/thunderjr/go-clean-api/src/infra/database/local/models"
@@ -10,32 +9,41 @@ import (
 
 var waitGroup sync.WaitGroup
 
-func handleModelsChannel(db *gorm.DB, modelChannel chan interface{}) {
+func handleModelsChannel(db *gorm.DB, modelChannel chan interface{}, doneChannel chan error) {
 	for model := range modelChannel {
 		waitGroup.Add(1)
 
 		go func(model interface{}) {
-			defer waitGroup.Done()
-			err := db.AutoMigrate(&model)
+			err := db.AutoMigrate(model)
+			waitGroup.Done()
 			if err != nil {
-				log.Println(err)
+				doneChannel <- err
 			}
 		}(model)
 	}
 }
 
-func Migrate(db *gorm.DB) {
-	models := []interface{}{
-		models.TaskModel{},
-		models.UserModel{},
+func Migrate(db *gorm.DB) error {
+	models := []any{
+		&models.TaskModel{},
+		&models.UserModel{},
 	}
 
 	modelChannel := make(chan interface{})
-	go handleModelsChannel(db, modelChannel)
+	doneChannel := make(chan error)
+	go handleModelsChannel(db, modelChannel, doneChannel)
 
 	for _, model := range models {
 		modelChannel <- model
 	}
 
 	waitGroup.Wait()
+	close(modelChannel)
+
+	select {
+	case err := <-doneChannel:
+		return err
+	default:
+		return nil
+	}
 }
