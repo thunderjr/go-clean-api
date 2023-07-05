fea10d57986f3d3a1d116cd211af20cdf7741708
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-07-05 00:25:05 -0300

    test: handle migrations

---
+++ src/infra/database/handle-migrations_test.go
@@ -0,0 +1,29 @@
+package infra_database_test
+
+import (
+	"os"
+	"testing"
+
+	infra_database "github.com/thunderjr/go-clean-api/src/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestMigrations(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("test_database.db"), &gorm.Config{})
+	if err != nil {
+		t.Error("Unable to connect to Test Database", err)
+	}
+
+	err = infra_database.Migrate(db)
+	if err != nil {
+		t.Error("Error migrating database: ", err)
+	}
+
+	defer func() {
+		err = os.Remove("test_database.db")
+		if err != nil {
+			t.Error("Unable to delete test_database.db file", err)
+		}
+	}()
+}
