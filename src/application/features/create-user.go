8d13f2cfc0e97f2cd19ec9d5d3492fc7b813d0ce
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-07-05 00:45:05 -0300

    feat: create User feature

---
+++ src/application/features/create-user.go
@@ -0,0 +1,26 @@
+package features
+
+import (
+	"github.com/thunderjr/go-clean-api/src/domain/entities"
+	"github.com/thunderjr/go-clean-api/src/domain/repositories"
+)
+
+type CreateUserFeature struct {
+	repository repositories.UserRepository
+}
+
+func GetCreateUserFeature(r repositories.UserRepository) *CreateUserFeature {
+	return &CreateUserFeature{
+		repository: r,
+	}
+}
+
+func (f CreateUserFeature) CreateUser(name string) (*entities.User, error) {
+	user := entities.User{
+		Name: name,
+	}
+
+	user = *f.repository.SaveUser(user)
+
+	return &user, nil
+}
