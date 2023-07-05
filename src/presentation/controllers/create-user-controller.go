41878dc86cac8340999f15abee7380bc5a6dbb78
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-07-05 00:46:21 -0300

    feat: create user controller

---
+++ src/presentation/controllers/create-user-controller.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/thunderjr/go-clean-api/src/application/features"
+	"github.com/thunderjr/go-clean-api/src/domain/entities"
+)
+
+type CreateUserController struct {
+	createUserFeature *features.CreateUserFeature
+}
+
+func GetCreateUserController(f *features.CreateUserFeature) Controller {
+	return &CreateUserController{
+		createUserFeature: f,
+	}
+}
+
+func (c *CreateUserController) Handle(data any) Response {
+	user, err := c.createUserFeature.CreateUser((data.(*entities.User)).Name)
+
+	if err != nil {
+		return Response{
+			Data: map[string]interface{}{
+				"message": err.Error(),
+			},
+			Status: http.StatusInternalServerError,
+		}
+	}
+
+	return Response{
+		Data:   user,
+		Status: http.StatusOK,
+	}
+}
