f4b34acceb2fa482e289d685c591a7b15c8e8a8b
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-07-06 01:08:54 -0300

    refactor: incremented the Router interface signature

--- src/application/router/router.go
+++ src/application/router/router.go
@@ -1,15 +1,13 @@
 package router
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/thunderjr/go-clean-api/src/presentation/controllers"
+)
 
-type Router struct {
-	server *echo.Echo
-}
-
-func Init(server *echo.Echo) error {
-	var router = &Router{server: server}
-
-	CreateUserRoute(router)
-
-	return nil
+type Router interface {
+	Handle(method string, path string, handler controllers.Controller) error
+	GET(path string, handler controllers.Controller) error
+	POST(path string, handler controllers.Controller) error
+	PUT(path string, handler controllers.Controller) error
+	DELETE(path string, handler controllers.Controller) error
 }
