0af6cd79a7958173f05a1ca5004a12dd8c8235ab
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-07-06 01:10:52 -0300

    refactor: create user route to use application router

--- src/application/router/user-routes.go
+++ src/application/router/user-routes.go
@@ -1,26 +1,16 @@
 package router
 
 import (
-	"github.com/labstack/echo/v4"
 	"github.com/thunderjr/go-clean-api/src/application/features"
-	"github.com/thunderjr/go-clean-api/src/domain/entities"
 	local_database "github.com/thunderjr/go-clean-api/src/infra/database/local"
 	local_repositories "github.com/thunderjr/go-clean-api/src/infra/database/local/repositories"
 	"github.com/thunderjr/go-clean-api/src/presentation/controllers"
 )
 
 func CreateUserRoute(router *Router) {
-	repository := local_repositories.GetLocalUserRepository(local_database.DB)
-	feature := features.GetCreateUserFeature(repository)
-	controller := controllers.GetCreateUserController(feature)
+	repository := local_repositories.NewLocalUserRepository(local_database.DB)
+	feature := features.NewCreateUserFeature(repository)
+	controller := controllers.NewCreateUserController(feature)
 
-	router.server.POST("/user", func(ctx echo.Context) error {
-		data := new(entities.User)
-		if err := ctx.Bind(data); err != nil {
-			return err
-		}
-
-		res := controller.Handle(data)
-		return ctx.JSON(res.Status, res.Data)
-	})
+	(*router).Handle("POST", "/user", controller)
 }
