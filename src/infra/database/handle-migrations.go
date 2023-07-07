f1b56f5b012104dd77b6f05440c58845a7a708bb
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-07-06 22:06:43 -0300

    fix: remove usage of go routines to migrate models | xD

--- src/infra/database/handle-migrations.go
+++ src/infra/database/handle-migrations.go
@@ -1,27 +1,9 @@
 package infra_database
 
 import (
-	"sync"
-
 	"github.com/thunderjr/go-clean-api/src/infra/database/local/models"
 	"gorm.io/gorm"
 )
-
-var waitGroup sync.WaitGroup
-
-func handleModelsChannel(db *gorm.DB, modelChannel chan interface{}, doneChannel chan error) {
-	for model := range modelChannel {
-		waitGroup.Add(1)
-
-		go func(model interface{}) {
-			err := db.AutoMigrate(model)
-			waitGroup.Done()
-			if err != nil {
-				doneChannel <- err
-			}
-		}(model)
-	}
-}
 
 func Migrate(db *gorm.DB) error {
 	models := []any{
@@ -29,23 +11,5 @@ func Migrate(db *gorm.DB) error {
 		&models.UserModel{},
 	}
 
-	modelChannel := make(chan interface{})
-	doneChannel := make(chan error)
-
-	go handleModelsChannel(db, modelChannel, doneChannel)
-	go handleModelsChannel(db, modelChannel, doneChannel)
-
-	for _, model := range models {
-		modelChannel <- model
-	}
-
-	close(modelChannel)
-	waitGroup.Wait()
-
-	select {
-	case err := <-doneChannel:
-		return err
-	default:
-		return nil
-	}
+	return db.AutoMigrate(models...)
 }
